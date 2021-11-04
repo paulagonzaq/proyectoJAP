6718078a7db8e41c9f40739a8016217540ca3d0f
paulagonzaq <paulagonzalezaquino@hotmail.com>
2021-11-04 18:52:24 -0300

    arreglo del login

--- js/login.js
+++ js/login.js
@@ -1,25 +1,31 @@
+const input = document.getElementsByTagName("button")[0]
+
+
+
+function login() {
+    input.addEventListener("click", () => {
+        let mail = document.getElementById("mail").value
+        let password = document.getElementById("password").value
+        let usuario = {};
+    
+        if (mail && password){
+    
+            usuario.nombre = mail;
+            usuario.estado = "Conectado";
+            localStorage.setItem('usuario',JSON.stringify(usuario));
+    
+            window.location.href = "./home.html"
+            
+        } else {
+            document.getElementById("warning").style.display = "block" 
+        }
+    })
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
-const input = document.getElementsByTagName("button")[0]
+login();
 
-input.addEventListener("click", () => {
-    let mail = document.getElementById("mail").value
-    let password = document.getElementById("password").value
-    let usuario = {};
-
-    if (mail && password){
-
-        usuario.nombre = mail;
-        usuario.estado = "Conectado";
-        localStorage.setItem('usuario',JSON.stringify(usuario));
-
-        window.location.href = "./home.html"
-        
-    } else {
-        document.getElementById("warning").style.display = "block" 
-    }
-})
 });
