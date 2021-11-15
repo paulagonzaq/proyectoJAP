a9f881e38217e27a299fc07f9bcf4ac83a9a18f4
paulagonzaq <paulagonzalezaquino@hotmail.com>
2021-11-14 23:27:33 -0300

    entrega 7 terminada

--- js/cart.js
+++ js/cart.js
@@ -89,3 +89,65 @@ document.getElementById("enviopremium").addEventListener("change", function(){
  })
 
 })
+
+var formaDePago = 0
+
+function mostrarTarjeta(){
+  div1 = document.getElementById('form-transferencia');
+  div1.style.display = 'none';
+  div = document.getElementById('form-tarjeta');
+  div.style.display = '';
+  formaDePago = 1
+}
+
+function mostrarTransferencia(){
+  div = document.getElementById('form-tarjeta');
+  div.style.display = 'none';
+  div1 = document.getElementById('form-transferencia');
+  div1.style.display = '';
+  formaDePago = 2
+}
+
+document.getElementById("comprar").addEventListener("click", function(){
+  let calle = document.getElementById("calle").value
+  let numero = document.getElementById("numerocalle").value
+  let esquina = document.getElementById("esquina").value
+  let pais = document.getElementById("pais").value
+  let contador = 0
+  // let tarjeta = document.getElementById("credit-card").value
+  // let transferencia = document.getElementById("transfer").value
+
+  
+  if (calle.length == "") {
+    alert("Introduce una calle")
+    contador+=1;
+  }
+  if (numero.length == "") {
+    alert("Introduce un número")
+    contador+=1;
+  }
+  if (esquina.length == "") {
+    alert("Introduce una esquina")
+    contador+=1;
+  }
+  if (pais.length == "") {
+    alert("Introduce un país")
+    contador+=1;
+  }
+  if (formaDePago == 0) {
+    alert("Introduce una forma de pago")
+    contador+=1;
+  }
+  
+  if (contador===0){
+    Swal.fire({
+      position: 'middle',
+      html: '<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_4eth4jy9.json"  background="transparent"  speed="1"  style="width: 450px; height: 450px;"    autoplay></lottie-player>',
+      title: '¡Compra realizada con éxito!',
+      showConfirmButton: false,
+      timer: 3000
+    })
+   }
+
+})
+
